adc4c30b8c9be08711dd98c69754594ff73862cb
James Robertson <james@jamesrobertson.me.uk>
2021-11-28 23:24:22 +0000

    feature: An XSPF playlist can now be generated.

--- got_mp3.gemspec
+++ got_mp3.gemspec
@@ -1,12 +1,14 @@
 Gem::Specification.new do |s|
   s.name = 'got_mp3'
-  s.version = '0.3.0'
+  s.version = '0.4.0'
   s.summary = 'A ruby-mp3info wrapper to make it convenient to update the ' +
       'album art and track titles of multiple MP3 files in a batch process.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/got_mp3.rb']
   s.add_runtime_dependency('dxlite', '~> 0.3', '>=0.3.5')
   s.add_runtime_dependency('ruby-mp3info', '~> 0.8', '>=0.8.10')
+  s.add_runtime_dependency('ruby-ogginfo', '~> 0.7', '>=0.7.2')
+  s.add_runtime_dependency('playlist_creator', '~> 0.2', '>=0.2.0')
   s.signing_key = '../privatekeys/got_mp3.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
--- lib/got_mp3.rb
+++ lib/got_mp3.rb
@@ -4,10 +4,14 @@
 
 require 'dxlite'
 require 'ostruct'
-require "mp3info"
+require 'mp3info'
+require 'ogginfo'
+require 'playlist_creator'
 
 
 class GotMP3
+
+  attr_accessor :dir
 
   def initialize(dir: '.', debug: false)
     @dir, @debug = dir, debug
@@ -44,7 +48,58 @@ class GotMP3
 
   end
 
-  # copy all MP3 directories through the category file-directory stucture
+  # Adds an XSPF playlist file to each file directory containing MP3s
+  #
+  def add_playlist(dir: @dir)
+
+    # .new('/home/james/Music2/Christmas)
+    #mp3s = Dir[File.join(@dir, '*.mp3')].sort_by {|x| File.mtime(x)}.reverse
+    a = Dir.glob(File.join(dir, '*')).select {|f| File.directory? f}
+
+    tracks = []
+
+    a.each do |dir|
+
+      tracks += add_playlist(dir: dir)
+
+      each_media_track(dir) do |media, trackno, filepath|
+
+        info = case File.extname(filepath)
+        when '.mp3'
+          mp3info(media)
+        when '.ogg'
+          ogginfo(media)
+        end
+
+        h = {
+          title: info[:title],
+          location: File.join(File.basename(dir), File.basename(filepath)),
+          album: info[:album]
+        }
+
+        tracks << h
+
+      end
+
+    end
+
+
+
+    pc = PlaylistCreator.new()
+
+    tracks.each do |x|
+      pc.add title: x[:title], location: x[:location], album: x[:album]
+    end
+
+    File.write File.join(dir,'playlist.xspf'), pc.to_xspf
+
+    return tracks
+
+  end
+
+  # Copies all MP3 directories through the category file-directory stucture.
+  # To accomplish this, it need a reference (source) directory containing the
+  # .txt for each album.
   #
   def compile(source_directory: '', target_directory: '')
 
@@ -72,6 +127,8 @@ class GotMP3
 
   end
 
+  # copy the .txt file from each mp3 directory into a central file directory
+  #
   def consolidate_txt(target_directory: '')
 
     raise 'target_directory cannot be empty' if target_directory.empty?
@@ -90,19 +147,19 @@ class GotMP3
 
   end
 
-  def each_mp3_file(directory='.', &blk)
+  def each_media_file(directory=@dir, &blk)
 
-    puts 'each_mp3 - directory: ' + directory.inspect if @debug
-    found = Dir[File.join(directory, "*.mp3")].sort_by { |x| File.mtime(x) }
-    puts 'each_mp3 - found: ' + found.inspect if @debug
+    puts 'each_media_file - directory: ' + directory.inspect if @debug
+    found = Dir[File.join(directory, "*.{mp3,ogg}")].sort_by { |x| File.mtime(x) }
+    puts 'each_nedia_file - found: ' + found.inspect if @debug
 
-    found.reverse.each.with_index do |mp3_filepath, i|
+    found.each.with_index do |media_filepath, i|
 
-      puts 'each_mp3 - mp3_filepath: ' + mp3_filepath.inspect if @debug
+      puts 'each_medi... - media_filepath: ' + media_filepath.inspect if @debug
 
-      next unless File.exists? mp3_filepath
+      next unless File.exists? media_filepath
 
-      blk.call(mp3_filepath, i )
+      blk.call(media_filepath, i )
 
     end
 
@@ -136,7 +193,7 @@ class GotMP3
   #
   def rename()
 
-    each_mp3_file do |mp3_filepath|
+    each_media_file do |mp3_filepath|
 
       mp3_directory = File.dirname(mp3_filepath)
       mp3_filename = File.basename(mp3_filepath)
@@ -148,11 +205,28 @@ class GotMP3
 
   end
 
+  # sort MP3 or OGG by track number in the filename
+  #
+  def sort_by_tracknum!()
+
+    each_media_file do |filepath|
+
+      directory = File.dirname(filepath)
+      a = Dir[File.join(directory, '*.{mp3,ogg}')]
+      a.sort_by {|x| x[/^\d+/]}.each {|file| FileUtils.touch file}
+
+    end
+
+  end
+
+  # write either a track listing into the media filepath, either in
+  # plain text or XML format.
+  #
   def write_titles(format: 'txt')
 
     puts 'inside write_titles()' if @debug
 
-    find_by_ext('.mp3').each do |directory, _ |
+    find_by_ext('.{mp3,ogg}').each do |directory, _ |
 
       puts 'write_titles() - directory: ' + directory.inspect if @debug
       txt_filename = Dir[File.join(directory, '*.txt')].first
@@ -161,17 +235,17 @@ class GotMP3
 
       tracks = []
 
-      each_mp3_track(directory) do |mp3, trackno, mp3_filepath|
+      each_media_track(directory) do |media, trackno, media_filepath|
 
-        tracks << OpenStruct.new({
-          title: mp3.tag.title.sub(/^\d+\. */,''),
-          artist: mp3.tag.artist,
-          album: mp3.tag.album,
-          album_artist: mp3.tag2['TPE2'] || mp3.tag.artist,
-          disc: mp3.tag2['TPOS'] || 1,
-          tracknum: mp3.tag.tracknum,
-          filename: File.basename(mp3_filepath)
-        })
+        info = case File.extname(media_filepath)
+        when '.mp3'
+          mp3info(media)
+        when '.ogg'
+          ogginfo(media)
+        end
+
+        info[:filename] = File.basename(media_filepath)
+        tracks << OpenStruct.new(info)
 
       end
 
@@ -180,10 +254,11 @@ class GotMP3
       heading = tracks[0].album_artist + ' - ' + tracks[0].album
 
       s = "# %s\n\n" % [heading]
-      h = tracks.group_by(&:disc)
 
 
       if format.to_sym == :txt then
+
+        h = tracks.group_by(&:disc)
 
         body = if h.length == 1 then
 
@@ -207,12 +282,15 @@ class GotMP3
         dx = DxLite.new('album[album_artist, album]/track(tracknum, title, ' +
                                                  'artist, disc, album_artist)')
 
-        h.each {|_,x| puts x.inspect } if @debug
-        h.each {|_,x| x.each {|track| dx.create(track.to_h) } }
+        #h.each {|_,x| puts x.inspect } if @debug
+        #h.each {|_,x| x.each {|track| dx.create(track.to_h) } }
+        tracks.each {|track| dx.create(track.to_h) }
 
         dx.album_artist = dx.all[0].album_artist
         dx.album = dx.all[0].album
         dx.save File.join(directory, 'playlist.dx')
+
+        yield(directory) if block_given?
 
       end
 
@@ -231,7 +309,7 @@ class GotMP3
     image_file = File.new(File.join(directory, img_filename),'rb')
     img = image_file.read
 
-    each_mp3_track(directory) do |mp3, _, _|
+    each_media_track(directory) do |mp3, _, _|
 
       mp3.tag2.remove_pictures
       mp3.tag2.add_picture img
@@ -240,14 +318,16 @@ class GotMP3
 
   end
 
+  # Modifies the MP3s' tags using the track titles in the .txt file
+  #
   def add_tracktitles(directory, txt_filename)
 
     txt_file = File.join(directory, txt_filename)
     track_titles = File.read(txt_file).lines[1..-1].map(&:strip)
 
-    each_mp3_track(directory) do |mp3, trackno, _|
+    each_media_track(directory) do |media, trackno, _|
 
-      mp3.tag.title = track_titles[trackno-1]
+      media.tag.title = track_titles[trackno-1]
 
     end
   end
@@ -285,11 +365,16 @@ class GotMP3
 
   end
 
-  def each_mp3_track(directory, &blk)
+  def each_media_track(directory, &blk)
 
-    each_mp3_file(directory) do |mp3_filepath, i|
+    each_media_file(directory) do |filepath, i|
 
-      Mp3Info.open(mp3_filepath) {|mp3|  blk.call(mp3, i+1, mp3_filepath) }
+      case File.extname(filepath)
+      when '.mp3'
+        Mp3Info.open(filepath) {|mp3|  blk.call(mp3, i+1, filepath) }
+      when '.ogg'
+        OggInfo.open(filepath) {|ogg|  blk.call(ogg, i+1, filepath) }
+      end
 
     end
 
@@ -309,6 +394,8 @@ class GotMP3
     end
   end
 
+  # used by public method *write_titles* to list the tracks in plain text
+  #
   def list(tracks)
 
     a = if tracks.map(&:artist).uniq.length < 2 then
@@ -319,6 +406,33 @@ class GotMP3
 
     a.join("\n")
 
+  end
+
+  def mp3info(mp3)
+    {
+      title: mp3.tag.title.sub(/^\d+\. */,''),
+      artist: mp3.tag.artist,
+      album: mp3.tag.album,
+      album_artist: mp3.tag2['TPE2'] || mp3.tag.artist,
+      disc: mp3.tag2['TPOS'] || 1,
+      tracknum: mp3.tag.tracknum,
+    }
+  end
+
+  def ogginfo(ogg)
+
+    # options
+    # ["title", "artist", "tracknumber", "tracktotal", "album", "albumartist",
+    #  "genre", "discid", "musicbrainz_discid"]
+
+    {
+      title: ogg.tag.title.sub(/^\d+\. */,''),
+      artist: ogg.tag.artist,
+      album: ogg.tag.album,
+      album_artist: ogg.tag.albumartist || ogg.tag.artist,
+      disc: ogg.tag.discid || 1,
+      tracknum: ogg.tag.tracknumber,
+    }
   end
 
 end
